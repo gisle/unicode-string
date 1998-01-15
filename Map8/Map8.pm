7e3e93fbe3ac40745ea858706993c3c2d93a7423
Gisle Aas <gisle@aas.no>
1998-01-15 18:39:34 +0000

    Added documentation.

--- Map8/Map8.pm
+++ Map8/Map8.pm
@@ -79,4 +79,72 @@ sub DESTROY
     $self->_free;
 }
 
+sub tou
+{
+    require Unicode::String;
+    my $self = shift;
+    Unicode::String::utf16($self->to16(@_));
+}
+
+# this should be done in C so that we don't have to allocate and UCS2 string
+# during the recoding process.
+
+sub recode8
+{
+    my $self = shift;
+    $_[0]->to8($self->map16($_[1]));
+}
+
 1;
+
+__END__
+
+=head1 NAME
+
+Unicode::Map8 - Mapping table between 8-bit chars and Unicode
+
+=head1 SYNOPSIS
+
+ require Unicode::Map8;
+ my $no_map = Unicode::Map8->new("ISO646-NO") || die;
+ my $l1_map = Unicode::Map8->new("latin1")    || die;
+
+ my $ustr = $no_map->to16("V}re norske tegn b|r {res");
+ my $lstr = $l1_map->to8($ustr);
+ print "$lstr\n";
+
+=head1 DESCRIPTION
+
+The Unicode::Map8 class implement efficient mapping tables between
+8-bit character sets and 16 bit character sets like Unicode.
+
+The following methods are available:
+
+=over 4
+
+=item $m = Unicode::Map->new( [$charset] )
+
+=item $m->addpair( $u8, $u16 );
+
+=item $m->nostrict;
+
+=item $m->to8( $ustr );
+
+=item $m->to16( $str );
+
+=item $m->tou( $str );
+
+=item $m->recode8($m2, $str);
+
+=item $m->fprint( FILE );
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright 1998 Gisle Aas.
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut

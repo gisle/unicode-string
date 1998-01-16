badf8839602a3f03da1ac25a6e8cabd1e65b4eaf
Gisle Aas <gisle@aas.no>
1998-01-16 13:21:49 +0000

    Added some content to the documentation.  Export some constants.

--- Map8/Map8.pm
+++ Map8/Map8.pm
@@ -6,10 +6,14 @@ package Unicode::Map8;
 # modify it under the same terms as Perl itself.
 
 use strict;
-use vars qw($VERSION @ISA $DEBUG $MAPS_DIR %ALIASES);
+use vars qw($VERSION @ISA @EXPORT_OK $DEBUG $MAPS_DIR %ALIASES);
 
 require DynaLoader;
 @ISA=qw(DynaLoader);
+
+require Exporter;
+*import = \&Exporter::import;
+@EXPORT_OK = qw(NOCHAR MAP8_BINFILE_MAGIC_HI MAP8_BINFILE_MAGIC_LO);
 
 $VERSION = '0.01';  # $Id$
 #$DEBUG++;
@@ -86,15 +90,6 @@ sub tou
     Unicode::String::utf16($self->to16(@_));
 }
 
-# this should be done in C so that we don't have to allocate and UCS2 string
-# during the recoding process.
-
-sub recode8
-{
-    my $self = shift;
-    $_[0]->to8($self->map16($_[1]));
-}
-
 1;
 
 __END__
@@ -116,29 +111,94 @@ Unicode::Map8 - Mapping table between 8-bit chars and Unicode
 =head1 DESCRIPTION
 
 The Unicode::Map8 class implement efficient mapping tables between
-8-bit character sets and 16 bit character sets like Unicode.
+8-bit character sets and 16 bit character sets like Unicode.  The
+16-bit strings is assumed to use network byte order.
 
 The following methods are available:
 
 =over 4
 
-=item $m = Unicode::Map->new( [$charset] )
+=item $m = Unicode::Map8->new( [$charset] )
+
+The object constructor creates new instances of the Unicode::Map8
+class.  I takes an optional argument that specify then name of a 8-bit
+character set to initialize from.  The argument can also be a the name
+of a mapping file.  If the charset/file can not be located, then the
+constructor returns I<undef>.
+
+If you omit the argument, then an empty mapping table is constructed.
+You must then add mapping pairs to it using the addpair() method
+described below.
 
 =item $m->addpair( $u8, $u16 );
 
+Adds a new mapping pair to the mapping object.  It takes two
+arguments.  The first is the code value in the 8-bit character set and
+the second is the corresponding code value in the 16-bit character
+set.  The same codes can be used multiple times (but not the same
+pair).  The first definition for a code is the one that is used.
+
+Concider the following example:
+
+  $m->addpair(0x20, 0x0020);
+  $m->addpair(0x20, 0x00A0);
+  $m->addpair(0xA0, 0x00A0);
+
+It means that the character 0x20 and 0xA0 in the 8-bit charset maps to
+themself in the 16-bit set, but in the 16-bit character set 0x0A0 maps
+to 0x20.
+
+=item $m->default_to8( $u8 )
+
+=item $m->default_to16( $u16 )
+
 =item $m->nostrict;
+
+All undefined mappings are replaced with the identity mapping.
+Undefined character are normally just zapped when converting between
+character sets.
 
 =item $m->to8( $ustr );
 
+Converts a 16-bit character string to the corresponding string in the
+8-bit character set.
+
 =item $m->to16( $str );
+
+Converts a 8-bit character string to the corresponding string in the
+16-bit character set.
 
 =item $m->tou( $str );
 
+Same an to16() but return a Unicode::String object instead of a plain
+UCS2 string.
+
 =item $m->recode8($m2, $str);
+
+Map the string $str from one 8-bit character set ($m) to another one
+($m2).  Since we know the mappings towards the common 16-bit encoding
+we can use this to convert between any of the 8-bit character sets we
+know about.
+
+=item $m->to_char16( $u8 )
+
+Maps an 8-bit character code to an 16-bit code.
+
+=item $m->to_char8( $u16 )
+
+Maps a 16-bit character code to an 8-bit code.
 
 =item $m->fprint( FILE );
 
+If the extention is compiled with the -DDEBUGGING option, then this
+method is available.  It prints a summary of the content of the
+mapping table on the specified file handle.
+
 =back
+
+=head1 BUGS
+
+Does not know how to handle Unicode surugates.
 
 =head1 COPYRIGHT
 

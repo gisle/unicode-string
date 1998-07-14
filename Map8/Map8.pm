0bbad9a6e285f53a4e0bf01849a717c66abccd0a
Gisle Aas <gisle@aas.no>
1998-07-14 11:13:35 +0000

    $VERSION is now 0.05

--- Map8/Map8.pm
+++ Map8/Map8.pm
@@ -15,7 +15,7 @@ require Exporter;
 *import = \&Exporter::import;
 @EXPORT_OK = qw(NOCHAR MAP8_BINFILE_MAGIC_HI MAP8_BINFILE_MAGIC_LO);
 
-$VERSION = '0.04';  # $Id$
+$VERSION = '0.05';  # $Id$
 #$DEBUG++;
 
 bootstrap Unicode::Map8 $VERSION;
